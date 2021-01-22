38dc23436285a793546ad57b50bb45dd353c7888
carlos perez <carlinchisart@gmail.com>
2021-01-22 11:26:46 -0500

    se crea el metodo index que retorna todos los registros para el recurso account

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
@@ -13,7 +14,7 @@ class AccountController extends Controller
      */
     public function index()
     {
-        //
+        return Account::all();
     }
 
     /**
