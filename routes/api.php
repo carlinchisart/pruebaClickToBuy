6fc6e855a60539461e61a4bb0a65039684766ad7
carlos perez <carlinchisart@gmail.com>
2021-01-22 14:17:31 -0500

    se crea la ruta api del recurso para los pedidos

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::resource('accounts',AccountController::class)->only(['index','store','show','update','destroy']);
+Route::resource('order', OrderController::class)->only(['store','update','destroy']);
