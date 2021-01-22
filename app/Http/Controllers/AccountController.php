0abca2c9e984f30bcca665c128d4a420b814a2a7
carlos perez <carlinchisart@gmail.com>
2021-01-22 13:48:40 -0500

    se realiza el metodo eliminar basico

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -90,8 +90,9 @@ class AccountController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Account $account)
     {
-        //
+        $account->delete();
+        return response()->json(['msg' => 'registro eliminado correctamente']);
     }
 }
